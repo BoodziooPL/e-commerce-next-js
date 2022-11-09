4284cb4d66f68189b4bfa1f21748e403f3a246d2
BoodziooPL <bogdanmalik@gmail.com>
2022-11-09 01:25:59 +0100

    commit

--- components/header.jsx
+++ components/header.jsx
@@ -7,9 +7,13 @@ function header() {
         <Image src={phone} />
       </div>
       <div className='flex-col space-y-1 px-5 ml-4 py-16 w-[100%] z-10 absolute md:ml-36'>
-        <p className='text-[1.1rem] md:text-[1.6rem]'>Lorem ipsum</p>
-        <p className='text-[1.6rem] md:text-[2.4rem]'>Lorem ipsum dolor</p>
-        <h1 className='text-[3.2rem] font-black text-white md:text-[4.6rem]'>
+        <p className='text-[1.1rem] md:text-[1.6rem text-gray-800 font-semibold lg:text-[2rem]'>
+          Lorem ipsum
+        </p>
+        <p className='text-[1.6rem] md:text-[2.4rem] text-gray-800 font-semibold lg:text-[3rem]'>
+          Lorem ipsum dolor
+        </p>
+        <h1 className='text-[3.2rem] font-black text-white md:text-[4.6rem] lg:text-[5rem]'>
           Header
         </h1>
         <button className='bg-red-400 text-white font-semibold rounded-full whitespace-nowrap px-5 py-3 text-[1.6rem] mt-5 md:absolute md:left-[50%] md:top-[60%] md:-translate-x-1/2 md:-translate-y-1/2 xl:top-[70%] xl:left-[30%] xl:-translate-y-3/4 hover:bg-transparent hover:border-red-400 border-solid hover:border-4'>
