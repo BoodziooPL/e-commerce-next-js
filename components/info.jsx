fc8429c6134dc17bdf997f180e6a0d23ec673b48
BoodziooPL <bogdanmalik@gmail.com>
2022-11-10 01:38:28 +0100

    roboto font

--- components/info.jsx
+++ components/info.jsx
@@ -6,7 +6,7 @@ import Image from 'next/image'
 
 function info() {
   return (
-    <div className=' bg-slate-200 h-[5rem] rounded-xl flex items-center md:h-[7rem]'>
+    <div className=' bg-slate-200 h-[5rem]  flex items-center md:h-[7rem]'>
       <div className='flex w-[100%] justify-around lg:text-4xl text-gray-700 text-lg'>
         <div className='flex items-center text-green-600 font-bold'>
           <IoIosContact className='text-4xl md:text-6xl' />
